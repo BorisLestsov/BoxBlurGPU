b324633ff166d8bbbb51a6fbf1bfeaccf8a359f5
Boris Lestsov <b.lestsov@corp.mail.ru>
2020-04-13 15:54:55 +0300

    read write

--- boxblur_cpu/source/main.cpp
+++ boxblur_cpu/source/main.cpp
@@ -1,24 +1,10 @@
-#include <cstdio>
-#include <cstdlib>
-#include <algorithm>
+#include <stdio.h>
+#include "common.h"
 
-int main(void)
+
+int main(int argc, char** argv)
 {
-  int N = 1<<20;
-  float *x, *y, *d_x, *d_y;
-  x = (float*)malloc(N*sizeof(float));
-  y = (float*)malloc(N*sizeof(float));
-
-  for (int i = 0; i < N; i++) {
-    x[i] = 1.0f;
-    y[i] = 2.0f;
-  }
-
-  float maxError = 0.0f;
-  for (int i = 0; i < N; i++)
-    maxError = std::max(maxError, std::abs((2*x[i] + y[i])-4.0f));
-  printf("Max error: %f\n", maxError);
-
-  free(x);
-  free(y);
+	PPMImage* img;
+	img = readPPM(argv[1]);
+	writePPM("res.ppm", img);
 }
