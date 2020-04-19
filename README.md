e6323d348e1a42c7fad4fc58c54a19be5cda977d
Boris Lestsov <b.lestsov@corp.mail.ru>
2020-04-19 23:05:30 +0300

    mode1 vs mode2

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # BoxBlur
+
+## Build:
+`mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release .. 
+make -j8`
+
+## Run:
+
+
--- benchmark/source/main.cpp
+++ benchmark/source/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <exception>
 #include "argparse.hpp"
 #include "box_filter_cpu.h"
@@ -13,8 +14,8 @@ int main(int argc, const char** argv)
         parser.addArgument("--input", 1, false);
         parser.addArgument("--output", 1, false);
         parser.addArgument("--ker", 1, false);
-        parser.addArgument("--cpumode", 1, false);
-        parser.addArgument("--cudamode", 1, false);
+        parser.addArgument("--mode1", 1, false);
+        parser.addArgument("--mode2", 1, false);
 
         parser.parse(argc, argv);
     } catch (std::exception e) {
@@ -27,33 +28,32 @@ int main(int argc, const char** argv)
         PPMImage* img, *res;
         img = readPPM(parser.retrieve<std::string>("input").c_str());
         int ker = parser.retrieve<int>("ker");
-        std::string cpu_mode = parser.retrieve<std::string>("cpumode");
-        std::string cuda_mode = parser.retrieve<std::string>("cudamode");
+        std::string mode1 = parser.retrieve<std::string>("mode1");
+        std::string mode2 = parser.retrieve<std::string>("mode2");
+        std::vector<std::string> modes = {mode1, mode2};
 
         res = new PPMImage;
         res->x = img->x - (ker - 1);
         res->y = img->y - (ker - 1);
         res->data = (PPMPixel*)malloc(res->x * res->y * sizeof(PPMPixel));
 
-        if (cpu_mode == "conv"){
-            box_blur_cpu(img, res, ker, 1);
-            box_blur_cpu(img, res, ker, 4);
-        } else if (cpu_mode == "sep") {
-            box_blur_cpu_sep(img, res, ker, 1);
-            box_blur_cpu_sep(img, res, ker, 4);
-        } else if (cpu_mode == "acc") {
-            box_blur_cpu_acc(img, res, ker, 1);
-            box_blur_cpu_acc(img, res, ker, 4);
-        } else {
-            throw std::string("Unknown cpu mode!");
-        }
-
-        if (cpu_mode == "conv"){
-            box_blur_cuda(img, res, ker);
-        } else if (cpu_mode == "sep") {
-            box_blur_cuda_sep(img, res, ker);
-        } else {
-            throw std::string("Unknown cuda mode!");
+        for (auto& mode: modes){
+            if (mode == "cpu_conv"){
+                box_blur_cpu(img, res, ker, 1);
+                box_blur_cpu(img, res, ker, 4);
+            } else if (mode == "cpu_sep") {
+                box_blur_cpu_sep(img, res, ker, 1);
+                box_blur_cpu_sep(img, res, ker, 4);
+            } else if (mode == "cpu_acc") {
+                box_blur_cpu_acc(img, res, ker, 1);
+                box_blur_cpu_acc(img, res, ker, 4);
+            } else if (mode == "cuda_conv"){
+                box_blur_cuda(img, res, ker);
+            } else if (mode == "cuda_sep") {
+                box_blur_cuda_sep(img, res, ker);
+            } else {
+                throw std::string("Unknown mode!: " + mode);
+            }
         }
 
         writePPM(parser.retrieve<std::string>("output").c_str(), res);
